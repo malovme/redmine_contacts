c5ac50346e0e9e0977fb8542cb0e041f35cffd45
Michael Malov <mmeemail@gmail.com>
2018-04-27 12:11:57 +0300

    functional tests for actions except index

--- test/functional/contacts_controller_test.rb
+++ test/functional/contacts_controller_test.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class ContactsControllerTest < ActionController::TestCase
 
-  fixtures :projects, :roles, :users
+  fixtures :projects, :roles, :users, :contacts
 
   def setup
     User.current = User.find(1)
@@ -17,13 +17,38 @@ class ContactsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  #test "should create article" do
-  #  assert_difference('Article.count') do
-  #    post :create, article: {title: 'Some title'}
-  #  end
+  test "should get new" do
+    get :new, project_id: @project.id
+    assert_response :success
+  end
 
-  #  assert_redirected_to article_path(assigns(:article))
-  #  assert_equal 'Article was successfully created.', flash[:notice]
-  #end
+  test "should create contact" do
+    assert_difference('Contact.count') do
+      post :create, project_id: @project.id, contact: { name: contacts(:john).name }
+    end
+    contact = assigns(:contact)
+    assert_redirected_to project_contacts_path(contact.project)
+    assert_match /#{contact.name}/, flash[:notice]
+  end
+
+  test "should show contact" do
+    get :show, id: contacts(:john).id
+    assert_response :success
+  end
+
+  test "should update contact" do
+    patch :update, id: contacts(:john).id, contact: { name: 'John updated' }
+    contact = assigns(:contact)
+    assert_equal 'John updated', contact.name
+    assert_redirected_to project_contacts_path(contact.project)
+  end
+
+  test "should destroy contact" do
+    contact = contacts(:john)
+    assert_difference('Contact.count', -1) do
+      delete :destroy, id: contact.id
+    end
+    assert_redirected_to project_contacts_path(contact.project)
+  end
 
 end
